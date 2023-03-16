d1fd00285c170322495122efa437f9f3f7fdfdf1
ana-karine <anakarine.ti@gmail.com>
2023-03-16 02:28:01 -0300

    add class Livro

--- 3 - Conhecendo a Linguagem de Programação Java/1 - Dominando IDEs Java/IntelliJ/Linux/primeiro_projeto_java_intellij/src/br/com/dio/PrimeiroProjeto.java
+++ 3 - Conhecendo a Linguagem de Programação Java/1 - Dominando IDEs Java/IntelliJ/Linux/primeiro_projeto_java_intellij/src/br/com/dio/PrimeiroProjeto.java
@@ -6,11 +6,47 @@ public class PrimeiroProjeto {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro = new Livro("Nome do livro", 100);
+        System.out.println(livro);
 
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a + b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numeroPaginas;
+
+    public Livro(String nome, Integer numeroPaginas) {
+        this.nome = nome;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(Integer numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numeroPaginas=" + numeroPaginas +
+                '}';
     }
 }
