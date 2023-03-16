d7f1c8eb33df712a90359e599501b3ab164f3a7e
ana-karine <anakarine.ti@gmail.com>
2023-03-16 13:21:11 -0300

    adicionando exemplos

--- 3 - Conhecendo a Linguagem de Programação Java/2 - Aprendendo a Sintaxe Java/java-tipos-variaveis/src/TiposVariaveis.java
+++ 3 - Conhecendo a Linguagem de Programação Java/2 - Aprendendo a Sintaxe Java/java-tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Ana Karine"; // a classe String representa texto na aplicação
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto1 = 1;
+        int numeroNormal = numeroCurto1;
+        short numeroCurto2 = (short)numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero); // 10
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
