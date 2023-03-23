0dc447de2249dcd9df9ef205ec3adcc605af75fb
ana-karine <anakarine.ti@gmail.com>
2023-03-23 18:11:24 -0300

    exercício pacotes

---
+++ 5 - Programação Orientada a Objetos com Java/pacotes/src/one/digitalinnovation/oo/ExemploPacotes.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.oo;
+
+import one.digitalinnovation.oo.outropacote.ExemploPacotes1;
+
+class ExemploPacotes {
+    ExemploPacotes1 exemploPacotes1;
+}
---
+++ 5 - Programação Orientada a Objetos com Java/pacotes/src/qualquer/outro/pacote/ExemploPacotes3.java
@@ -0,0 +1,7 @@
+package qualquer.outro.pacote;
+
+import one.digitalinnovation.oo.outropacote.ExemploPacotes1;
+
+public class ExemploPacotes3 {
+    ExemploPacotes1 exemploPacotes1;
+}
