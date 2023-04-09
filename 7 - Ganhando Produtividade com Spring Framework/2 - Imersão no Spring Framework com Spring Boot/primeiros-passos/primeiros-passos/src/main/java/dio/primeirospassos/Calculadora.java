e2f8cc4fd69d594d4417516403f16ab5db9840b0
ana-karine <anakarine.ti@gmail.com>
2023-04-09 16:19:54 -0300

    primeiros passos

---
+++ 7 - Ganhando Produtividade com Spring Framework/2 - Imersão no Spring Framework com Spring Boot/primeiros-passos/primeiros-passos/src/main/java/dio/primeirospassos/Calculadora.java
@@ -0,0 +1,11 @@
+package dio.primeirospassos;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+
+    public int somar(int num1, int num2) {
+        return num1 + num2;
+    }
+}
