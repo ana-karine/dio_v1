0790145471168296c22853afc38962955863ec13
ana-karine <anakarine.ti@gmail.com>
2023-04-10 01:46:34 -0300

    Properties Value

---
+++ 7 - Ganhando Produtividade com Spring Framework/2 - Imersão no Spring Framework com Spring Boot/properties-value/src/main/java/dio/springboot/PropertiesValueApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot;
+
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesValueApplication.class, args);
+	}
+
+}
