a54895470c2fe53bc29519fcd49960c42262444d
ana-karine <anakarine.ti@gmail.com>
2023-04-10 02:14:21 -0300

    Configuration Properties

---
+++ 7 - Ganhando Produtividade com Spring Framework/2 - Imersão no Spring Framework com Spring Boot/config-properties/src/main/java/dio/springboot/ConfigPropertiesApplication.java
@@ -0,0 +1,14 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigPropertiesApplication.class, args);
+	}
+
+}
+
